c92df460dd3ed0cd4e31a06b6f989771412ba883
HaruSari <uxdx159@gmail.com>
2020-04-23 03:28:58 +0900

    +FAB

--- js/init.js
+++ js/init.js
@@ -3,5 +3,12 @@
 
         $('.sidenav').sidenav();
         $('.dropdown-trigger').dropdown();
+        $(document).ready(function() {
+            $('.fixed-action-btn').floatingActionButton();
+        });
+        $('.fixed-action-btn').floatingActionButton({
+            toolbarEnabled: true
+        });
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
