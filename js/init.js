27216ab6c0294cd1efe63d013834a9a8476e924e
HaruSari <uxdx159@gmail.com>
2020-04-22 23:10:44 +0900

    init

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function($) {
+    $(function() {
+
+        $('.sidenav').sidenav();
+        $('.dropdown-trigger').dropdown();
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
